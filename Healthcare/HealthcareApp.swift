38e885e9697f80cfc88c661474bfa390f0d6369d
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-09-13 13:36:14 +0300

    Initial Commit

---
+++ Healthcare/HealthcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthcareApp.swift
+//  Healthcare
+//
+//  Created by ceksi on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
